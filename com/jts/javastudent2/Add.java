a2221995f07aa982aadc6df6a5139c7d8b52110b
Rinkoo Kushwah <rinkoobangalore@gmail.com>
2022-09-24 12:14:02 +0000

    Updated the code for Add.java

--- com/jts/javastudent2/Add.java
+++ com/jts/javastudent2/Add.java
@@ -12,12 +12,13 @@ public class Add{
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the value of a");
         int a = sc.nextInt();
+        System.out.println("Please enter the value of b");
         int b = sc.nextInt();
-
         int sum = addNumber(a, b);
-
-        System.out.println(sum);
+        System.out.println("Sum="+sum);
     
     }
 
